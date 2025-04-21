6c321ffcea693c1374c31b936a705aa72ba830a3
naihapi <2448428982@qq.com>
2025-04-21 11:46:11 +0800

    优化了main

--- main.ino
+++ main.ino
@@ -6,7 +6,6 @@ void setup()
 {
     WS2812_InitPro();
 
-    // LEDBoard_DisplayBackGround(DarkBlue);
     LEDBoard_DisplayGradualBackGround(0xCD00CD, 0x8B0A50, 0x8B0A50, NavyBlue);
     LEDBoard_DisplayPoint(DarkOrange);
     LEDBoard_DisplayMonth(10, Yellow);
